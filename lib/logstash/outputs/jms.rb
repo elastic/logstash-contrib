c530f87fb9994a5af2d2525c7e912618aaa7336a
Nicholas Omann <AlphaCluster@live.com>
2014-02-23 15:58:18 -0600

    Caching producer on output and new selector for input
    Made changes so that a producers will be cached when using JMS for an
    output. Also added the option to use a selector on an input. Also
    adjusted tabbing which is why there are more changes.

--- lib/logstash/outputs/jms.rb
+++ lib/logstash/outputs/jms.rb
@@ -7,15 +7,15 @@ require "logstash/namespace"
 # For more information about Jms, see <http://docs.oracle.com/javaee/6/tutorial/doc/bncdq.html>
 # For more information about the Ruby Gem used, see <http://github.com/reidmorrison/jruby-jms>
 # Here is a config example :
-#   jms {
-#      include_header => false
-#      include_properties => false
-#      include_body => true
-#      use_jms_timestamp => false
-#      queue_name => "myqueue"
-#      yaml_file => "~/jms.yml"
-#      yaml_section => "mybroker"
-#    }
+#	 jms {
+#			include_header => false
+#			include_properties => false
+#			include_body => true
+#			use_jms_timestamp => false
+#			queue_name => "myqueue"
+#			yaml_file => "~/jms.yml"
+#			yaml_section => "mybroker"
+#		}
 #
 #
 class LogStash::Outputs::Jms < LogStash::Outputs::Base
@@ -84,63 +84,64 @@ config :jndi_context, :validate => :hash
 	public
 	def register
 		require "jms"
-    @connection = nil
+		@connection = nil
 
-    if @yaml_file
-    	@jms_config = YAML.load_file(@yaml_file)[@yaml_section]
+		if @yaml_file
+				@jms_config = YAML.load_file(@yaml_file)[@yaml_section]
 
-    # TODO(claveau): causes an exception
-    # #<TypeError: can't dup NilClass> in /jms/connection.rb:172 (params.dup)
-    elsif @jndi_name = {
-    		:require_jars => @require_jars,
-    		:jndi_name => @jndi_name,
-    		:jndi_context => @jndi_context}
+		# TODO(claveau): causes an exception
+		# #<TypeError: can't dup NilClass> in /jms/connection.rb:172 (params.dup)
+		elsif @jndi_name = {
+				:require_jars => @require_jars,
+				:jndi_name => @jndi_name,
+				:jndi_context => @jndi_context}
 
-    elsif @factory
-    	@jms_config = {
-    		:require_jars => @require_jars,
-    		:factory => @factory,
-    		:username => @username,
-    		:password => @password,
-    		:broker_url => @broker_url,
-    		:url => @broker_url # "broker_url" is named "url" with Oracle AQ
-    		}
-    end
-    @logger.debug @jms_config
+		elsif @factory
+			@jms_config = {
+				:require_jars => @require_jars,
+				:factory => @factory,
+				:username => @username,
+				:password => @password,
+				:broker_url => @broker_url,
+				:url => @broker_url # "broker_url" is named "url" with Oracle AQ
+				}
+		end
+		@logger.debug @jms_config
 
-# Setup ConnectionPool
-connection = JMS::Connection.new(@jms_config)
-@jms_session_pool = connection.create_session_pool(@jms_config)
+		@connection = JMS::Connection.new(@jms_config)
+		@session = @connection.create_session()
 
-#TODO(AlphaCluster) Attempting producer caching... More work needed.
-#		connection = JMS::Connection.new(@jms_config)
-#    session = connection.create_session()
-#
-#    if @queue_name.nil?
-#	    @producer = session.producer(:queue_name => @queue_name)
-#	  else
-#		  @producer = session.create_producer(session.create_destination(:topic_name => @topic_name))
-#		end
-#    if !@delivery_mode.nil?
-#	    @producer.delivery_mode_sym = @deliver_mode
-#	  end
+		# Cache the producer since we should keep reusing this one.
+		if @queue_name.nil?
+			@producer = @session.create_producer(@session.create_destination(:topic_name => @topic_name))
+		else
+			@producer = @session.create_producer(@session.create_destination(:queue_name => @queue_name))
+		end
+
+		if !@delivery_mode.nil?
+			@producer.delivery_mode_sym = @deliver_mode
+		end
 	end # def register
 
 	def receive(event)
 			return unless output?(event)
 
 			begin
-				#@producer.send(session.message(event.to_json))
-				if @queue_name
-					@jms_session_pool.producer(:queue_name => @queue_name) do |session, producer|
-						producer.send(session.message("Hello World"))
-					end
-				else
-					@jms_session_pool.producer(:topic_name => @topic_name) do |session, producer|
-						producer.send(session.message("Hello World"))
-					end
-				end
+				@producer.send(@session.message(event.to_json))
+#				if @queue_name
+#					@jms_session_pool.producer(:queue_name => @queue_name) do |session, producer|
+#						producer.send(session.message("Hello World"))
+#					end
+#				else
+#					@jms_session_pool.producer(:topic_name => @topic_name) do |session, producer|
+#						producer.send(session.message("Hello World"))
+#					end
+#				end
 
+			rescue LogStash::ShutdownSignal => e
+				@producer.close()
+				@session.close()
+				@connection.close()
 			rescue => e
 				@logger.warn("Failed to send event to JMS", :event => event, :exception => e,
 										 :backtrace => e.backtrace)
