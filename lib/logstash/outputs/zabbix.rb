cd05914db4668d84944eacfdf94b08cb9344e78c
Colin Surprenant <colin.surprenant@gmail.com>
2014-06-04 19:24:08 -0400

    fix popen invocation parameters and some cleanups

--- lib/logstash/outputs/zabbix.rb
+++ lib/logstash/outputs/zabbix.rb
@@ -12,7 +12,7 @@ require "shellwords"
 # * "zabbix_item"    (the item key on the host in Zabbix)
 # * "send_field"    (the field name that is sending to Zabbix)
 #
-# In Zabbix, create your host with the same name (no spaces in the name of 
+# In Zabbix, create your host with the same name (no spaces in the name of
 # the host supported) and create your item with the specified key as a
 # Zabbix Trapper item. Also you need to set field that will be send to zabbix
 # as item.value, otherwise @message wiil be sent.
@@ -43,7 +43,7 @@ require "shellwords"
 #          ]
 #       }
 #     }
-#      
+#
 #     output {
 #       zabbix {
 #         # only process events with this tag
@@ -85,79 +85,63 @@ class LogStash::Outputs::Zabbix < LogStash::Outputs::Base
       return
     end
 
-    host = event["zabbix_host"]
-    if !host
+    host = Array(event["zabbix_host"])
+    if host.empty?
       @logger.warn("Skipping zabbix output; zabbix_host field is missing",
                    :missed_event => event)
       return
     end
 
-    item = event["zabbix_item"]
-    if !item
+    item = Array(event["zabbix_item"])
+    if item.empty?
       @logger.warn("Skipping zabbix output; zabbix_item field is missing",
                    :missed_event => event)
       return
     end
 
-    field = event["send_field"]
-    if !field
-      field = "message"
+    field = Array(event["send_field"])
+    if field.empty?
+      field = ["message"]
     end
 
-    host = [host] if host.is_a?(String)
-    item = [item] if item.is_a?(String)
-    field = [field] if field.is_a?(String)
+    item.each_with_index do |key, index|
 
-    if host.is_a?(Array) && item.is_a?(Array) && field.is_a?(Array) 
-      item.each_with_index do |key, index|
+      if field[index].nil? || (zmsg = event[field[index]]).nil?
+        @logger.warn("No zabbix message to send in event field #{field[index].inspect}", :field => field, :index => index, :event => event)
+        next
+      end
+
+      cmd = [@zabbix_sender, "-z", @host, "-p", @port, "-s", host[index].to_s, "-k", item[index].to_s, "-o", zmsg.to_s, "-v"]
+
+      @logger.debug("Running zabbix command", :command => cmd.join(" "))
+
+      begin
+        f = IO.popen(cmd, "r")
+
+        command_output = f.gets
+        command_processed = command_output[/processed: (\d+)/, 1]
+        command_failed = command_output[/failed: (\d+)/, 1]
+        command_total = command_output[/total: (\d+)/, 1]
+        command_seconds_spent = command_output[/seconds spent: ([\d\.]+)/, 1]
+
+        @logger.info("Message was sent to zabbix server",
+                     :command => cmd, :event => event,
+                     :command_processed => command_processed,
+                     :command_failed => command_failed,
+                     :command_total => command_total,
+                     :command_seconds_spent => command_seconds_spent)
+      rescue => e
+        @logger.warn("Skipping zabbix output; error calling zabbix_sender",
+                     :command => cmd, :missed_event => event,
+                     :exception => e, :backtrace => e.backtrace)
+      ensure
         begin
-          zmsg = event[field[index]]
-          zmsg = Shellwords.shellescape(zmsg)
+          f.close unless f.closed?
         rescue => e
-          @logger.warn("Error during receiving message for sending",
-                       :event => event,
-                       :exception => e, :backtrace => e.backtrace)
-        end
-
-        cmd = "#{@zabbix_sender} -z #{@host} -p #{@port} -s #{host[index]} -k #{item[index]} -o \"#{zmsg}\" -v"
-
-        @logger.debug("Running zabbix command", :command => cmd)
-
-        begin
-          f = IO.popen(cmd, "a+")
-          f.close_write unless f.closed?
-
-          command_output = f.gets
-          command_processed = command_output[/processed: (\d+)/,1]
-          command_failed = command_output[/failed: (\d+)/,1]
-          command_total = command_output[/total: (\d+)/,1]
-          command_seconds_spent = command_output[/seconds spent: ([\d\.]+)/,1]
-
-          @logger.info("Message was sent to zabbix server",
-                       :command => cmd, :event => event,
-                       :command_processed => command_processed,
-                       :command_failed => command_failed,
-                       :command_total => command_total,
-                       :command_seconds_spent => command_seconds_spent)
-        rescue => e
-          @logger.warn("Skipping zabbix output; error calling zabbix_sender",
-                       :command => cmd, :missed_event => event,
-                       :exception => e, :backtrace => e.backtrace)
-        ensure
-          begin
-            @logger.debug("Checking zabbix_sender command closing status", 
-                         :event => event, :command_status => f.closed?)
-            f.close unless f.closed?
-          rescue => e
-            @logger.warn("Error during closing zabbix_sender subprocess",
-                       :exception => e, :backtrace => e.backtrace)
-          end
+          @logger.warn("Error during closing zabbix_sender subprocess",
+                     :exception => e, :backtrace => e.backtrace)
         end
       end
-    else
-      @logger.warn("Skipping zabbix output; some issues with zabbix parameters conversion",
-                   :missed_event => event, :zabbix_host => host,
-                   :zabbix_item => item, :send_field => field)
     end
   end # def receive
 end # class LogStash::Outputs::Zabbix
